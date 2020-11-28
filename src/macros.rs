a96241db8ed758535430d61e8c9c8d32d660c576
YOSHIOKA Takuma <lo48576@hard-wi.red>
2020-11-28 23:09:57 +0900

    Implement `From<&str> for Box<custom_str>`

--- src/macros.rs
+++ src/macros.rs
@@ -26,6 +26,14 @@ macro_rules! impl_cmp_symmetric {
     };
 }
 
+/// Implements std traits for a custom string slice type.
+///
+/// # Safety
+///
+/// `$custom_str` type should have the same memory layout as `str`.
+/// For example, this is safe when `$custom_str` is defined as
+/// `#[repr(transparent)] struct $custom_str(str);`.
+/// The caller of this macro is responsible to satisfy this condition.
 macro_rules! impl_traits_for_custom_string_slice {
     ($custom_str:ty) => {
         impl_cmp_symmetric!($custom_str, str);
@@ -69,6 +77,25 @@ macro_rules! impl_traits_for_custom_string_slice {
             #[inline]
             fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
                 f.write_str(self.as_ref())
+            }
+        }
+
+        #[cfg(feature = "alloc")]
+        impl From<&$custom_str> for $crate::alloc::boxed::Box<$custom_str> {
+            fn from(s: &$custom_str) -> Self {
+                use $crate::alloc::boxed::Box;
+
+                let inner = s.as_str();
+                let inner_boxed = Box::<str>::from(inner);
+                unsafe {
+                    // This is safe when `$custom_str` has the same memory layout as `str`.
+                    // For example, this is safe when `$custom_str` is defined as
+                    // `#[repr(transparent)] struct $custom_str(str);`.
+                    // The caller of this macro is responsible to satisfy this condition.
+                    Box::<$custom_str>::from_raw(
+                        Box::<str>::into_raw(inner_boxed) as *mut $custom_str
+                    )
+                }
             }
         }
     };
